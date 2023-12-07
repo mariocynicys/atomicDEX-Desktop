5c6738bbc8d84818c4d606f239967281a98891be
cipig <cipianon@gmail.com>
2023-12-07 17:38:45 +0000

    my seeds + stats

--- src/core/atomicdex/config/mm2.cfg.hpp
+++ src/core/atomicdex/config/mm2.cfg.hpp
@@ -36,8 +36,8 @@ namespace atomic_dex
         int64_t                  rpcport{atomic_dex::g_dex_rpcport};
         int64_t                  metrics_interval{43200};
         //std::vector<std::string> seednodes{};
-        //std::vector<std::string> seednodes{"46.4.78.11", "46.4.87.18", "seed7.komodo.earth"};
-        std::vector<std::string> seednodes{"46.4.78.11", "46.4.87.18", "38.91.100.29", "162.55.5.83", "2.56.154.200"};
+        std::vector<std::string> seednodes{"46.4.78.11", "46.4.87.18", "168.119.236.246"};
+        //std::vector<std::string> seednodes{"46.4.78.11", "46.4.87.18", "38.91.100.29", "162.55.5.83", "2.56.154.200"};
 #ifdef _WIN32
         std::string userhome{utils::u8string(std::filesystem::path(_wgetenv(L"HOMEPATH")))};
 #else
